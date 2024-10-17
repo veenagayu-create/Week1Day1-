42b4554df90ed3b65735fc86b1ceb890bad336de
veenagayu-create <veena.gayathiri5@gmail.com>
2024-10-17 12:19:10 +0530

    Add files via upload
    Week1Day1 Changes

---
+++ Chrome.java
@@ -0,0 +1,21 @@
+package Week1Day1;
+
+public class Chrome {
+
+	public static void main(String[] args) {
+		//declare the variables
+		float version = 91.0f;
+		String developer="Google";
+		boolean isBeta=false;
+		int releaseYear=2008;
+		char shortcutKey='C';
+		
+		System.out.println(version);
+		System.out.println(developer);
+		System.out.println(isBeta);
+		System.out.println(releaseYear);
+		System.out.println(shortcutKey);
+		
+	}
+
+}
